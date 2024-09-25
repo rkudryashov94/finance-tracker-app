b1ebc17c5efb0fa400fa3940414019ce4be5b22b
Roman Kudryashov <romankudryashov94@gmail.com>
2024-09-25 15:18:44 -0700

    Integrate with Firebase Firestore & Google Verification
    - Installed Firebase dependency
    - Integrated with Firebase Firestore
    - Added Google Verification tag
    - Updated meta tag keywords
    - Added vercel.json to override Vercel no indexing

--- app/layout.js
+++ app/layout.js
@@ -4,33 +4,68 @@ import Nav from "@/components/Navigation";
 
 // Metadata for SEO
 export const metadata = {
-  title: "Finance Tracker App",
-  description:
-    "Easily track your income and expenses, and manage your finances effectively.",
+	title: "Finance Tracker App",
+	description:
+		"Easily track your income and expenses, and manage your finances effectively.",
 
-  // Additional SEO metadata
-  keywords: [
-    "Expense tracker",
-    "Budgeting",
-    "Personal finance",
-    "Income and expenses",
-  ],
-  author: "Roman Kudryashov",
+	// Additional SEO metadata
+	keywords: [
+		"expense tracker",
+		"budgeting",
+		"personal finance",
+		"expense management",
+		"financial planning",
+		"track expenses",
+		"save money",
+		"budgeting app",
+		"income and expenses",
+	],
+	author: "Roman Kudryashov",
+	verification: {
+		google: "6IFdr087Tj-ReSBlc3qYvQzTBVlC7Pnc3_-7Ss-_vTw",
+	},
 };
 
 // Viewport configuration for mobile optimization
 export const viewport = {
-  width: "device-width",
-  initialScale: 1.0,
+	width: "device-width",
+	initialScale: 1.0,
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Nav />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<title>Finance Tracker App</title>
+				<meta
+					name="description"
+					content="Easily track your income and expenses, and manage your finances effectively."
+				/>
+				<meta
+					name="keywords"
+					content={[
+						"expense tracker",
+						"budgeting",
+						"personal finance",
+						"expense management",
+						"financial planning",
+						"track expenses",
+						"save money",
+						"budgeting app",
+						"income and expenses",
+					]}
+				/>
+				<meta name="author" content="Roman Kudryashov" />
+				<meta
+					name="google-site-verification"
+					content="6IFdr087Tj-ReSBlc3qYvQzTBVlC7Pnc3_-7Ss-_vTw"
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			</head>
+			<body>
+				<Nav />
+				{children}
+			</body>
+		</html>
+	);
 }
--- app/page.js
+++ app/page.js
@@ -13,107 +13,109 @@ import { Doughnut } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DUMMY_DATA = [
-  {
-    id: 1,
-    title: "Entertainment",
-    color: "yellow",
-    total: 500,
-  },
+	{
+		id: 1,
+		title: "Entertainment",
+		color: "yellow",
+		total: 500,
+	},
 
-  {
-    id: 2,
-    title: "Utilities",
-    color: "blue",
-    total: 400,
-  },
+	{
+		id: 2,
+		title: "Utilities",
+		color: "blue",
+		total: 400,
+	},
 
-  {
-    id: 3,
-    title: "Fuel",
-    color: "red",
-    total: 300,
-  },
+	{
+		id: 3,
+		title: "Fuel",
+		color: "red",
+		total: 300,
+	},
 
-  {
-    id: 4,
-    title: "Movies",
-    color: "green",
-    total: 100,
-  },
+	{
+		id: 4,
+		title: "Movies",
+		color: "green",
+		total: 100,
+	},
 
-  {
-    id: 5,
-    title: "Holidays",
-    color: "purple",
-    total: 2000,
-  },
+	{
+		id: 5,
+		title: "Holidays",
+		color: "purple",
+		total: 2000,
+	},
 ];
 
 export default function Home() {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [showAddIncomeModal, setShowAddIncomeModal] = useState(false);
 
-  return (
-    <>
-      {/* Modal */}
-      <Modal show={modalIsOpen} onClose={setModalIsOpen}>
-        <h3>I am a Modal Test</h3>
-      </Modal>
+	return (
+		<>
+			{/* Modal */}
+			<Modal show={showAddIncomeModal} onClose={setShowAddIncomeModal}>
+				<h3>I am a Modal Test</h3>
+			</Modal>
 
-      <main className="container max-w-2xl px-6 mx-auto">
-        {/* Account Balance */}
-        <section className="py-3">
-          <small className="text-gray-400 text-md">My Balance</small>
-          <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
-        </section>
+			<main className="container max-w-2xl px-6 mx-auto">
+				{/* Account Balance */}
+				<section className="py-3">
+					<small className="text-gray-400 text-md">My Balance</small>
+					<h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
+				</section>
 
-        {/* Add Expenses & Add Income buttons */}
-        <section className="flex items-center gap-2 py-3">
-          <button
-            onClick={() => setModalIsOpen(true)}
-            className="btn btn-primary"
-          >
-            + Expenses
-          </button>
-          <button className="btn btn-primary-outline">+ Income</button>
-        </section>
+				{/* Add Expenses & Add Income buttons */}
+				<section className="flex items-center gap-2 py-3">
+					<button onClick={() => {}} className="btn btn-primary">
+						+ Expenses
+					</button>
+					<button
+						onClick={() => setShowAddIncomeModal(true)}
+						className="btn btn-primary-outline"
+					>
+						+ Income
+					</button>
+				</section>
 
-        {/* Expenses */}
-        <section className="py-6">
-          <h3 className="text-2xl">My Expenses</h3>
-          <div className="flex flex-col gap-4 mt-6">
-            {DUMMY_DATA.map((expense) => {
-              return (
-                <ExpenseCategoryItem
-                  color={expense.color}
-                  title={expense.title}
-                  total={expense.total}
-                />
-              );
-            })}
-          </div>
-        </section>
+				{/* Expenses */}
+				<section className="py-6">
+					<h3 className="text-2xl">My Expenses</h3>
+					<div className="flex flex-col gap-4 mt-6">
+						{DUMMY_DATA.map((expense) => {
+							return (
+								<ExpenseCategoryItem
+									color={expense.color}
+									title={expense.title}
+									total={expense.total}
+								/>
+							);
+						})}
+					</div>
+				</section>
 
-        {/* Chart section */}
-        <section className="py-6">
-          <h3 className="text-2xl">Stats</h3>
-          <div className="w-1/2 mx-auto">
-            <Doughnut
-              data={{
-                labels: DUMMY_DATA.map((expense) => expense.title),
-                datasets: [
-                  {
-                    label: "Expenses",
-                    data: DUMMY_DATA.map((expense) => expense.total),
-                    backgroundColor: DUMMY_DATA.map((expense) => expense.color),
-                    borderColor: ["#18181b"],
-                    borderWidth: 5,
-                  },
-                ],
-              }}
-            />
-          </div>
-        </section>
-      </main>
-    </>
-  );
+				{/* Chart section */}
+				<section className="py-6">
+					<h3 className="text-2xl">Stats</h3>
+					<div className="w-1/2 mx-auto">
+						<Doughnut
+							data={{
+								labels: DUMMY_DATA.map((expense) => expense.title),
+								datasets: [
+									{
+										label: "Expenses",
+										data: DUMMY_DATA.map((expense) => expense.total),
+										backgroundColor: DUMMY_DATA.map((expense) => expense.color),
+										borderColor: ["#18181b"],
+										borderWidth: 5,
+									},
+								],
+							}}
+						/>
+					</div>
+				</section>
+			</main>
+		</>
+	);
 }
