f4809ab37167f4b744dc73daa317bb68bd398804
Roman Kudryashov <romankudryashov94@gmail.com>
2024-09-30 00:26:57 -0700

    Finished ViewExpenseModal
    - Created ViewExpenseModal that shows all expenses within Expense Category
    - Implemented functionality to delete individual expense
    - Implemented functionality to delete Expense Category

--- components/ExpenseCategoryItem.js
+++ components/ExpenseCategoryItem.js
@@ -1,20 +1,29 @@
-import { currencyFormatter } from "@/lib/utils";
+import { useState } from "react";
 
-function ExpsenseCategoryItem({ color, title, total }) {
+import { currencyFormatter } from "@/lib/utils";
+import ViewExpenseModal from "@/components/modals/ViewExpenseModal"
+
+function ExpenseCategoryItem({ expense }) {
+
+  const [showViewExpenseModal, setShowViewExpenseModal] = useState(false)
+
   return (
-    <button>
+    <>
+    <ViewExpenseModal show={showViewExpenseModal} onClose={setShowViewExpenseModal} expense={expense}/>
+    <button onClick={() => setShowViewExpenseModal(true)}>
       <div className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
         <div className="flex items-center gap-2">
           <div
             className="w-[25px] h-[25px] rounded-full"
-            style={{ backgroundColor: color }}
+            style={{ backgroundColor: expense.color }}
           />
-          <h4 className="capitalize">{title}</h4>
+          <h4 className="capitalize">{expense.title}</h4>
         </div>
-        <p>{currencyFormatter(total)}</p>
+        <p>{currencyFormatter(expense.total)}</p>
       </div>
     </button>
+    </>
   );
 }
 
-export default ExpsenseCategoryItem;
+export default ExpenseCategoryItem;
